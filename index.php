e02e3d903e1db4e80ce73fcf15a6c4fcfee84106
Praweenporn <71056576+Praweenporn@users.noreply.github.com>
2021-08-20 14:01:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title>Hello, world!</title>
-    <script
-    src="https://code.jquery.com/jquery-2.2.4.js"
-    integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
-    crossorigin="anonymous">
-    </script>
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
   </head>
   <body>  
     <h2> Praweenporn Mathurot</h2>
@@ -32,24 +28,27 @@
             </div>
           </div>
         </div>
+        last update <span id="lastupdate"></span>
       </div>
     </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
   </body>
-   <script>
-       /*$(()=>{
-           //alert("Hello");
-           let url = "https://api.thingspeak.com/channels/1458414/feeds.json?results=1";
-           $.getJSON(url)
-            .done(funtion(data){
-              let feeds = data.feeds;
-              console.log(feeds[1]);
-              console.log(feeds[1].field1);
-            })
-            .fail(function(error){
 
-            });
-       });*/
+   <script>
+       $(()=>{
+           let url = "https://api.thingspeak.com/channels/1458414/feeds.json?results=1";
+           $.getJSON(url){
+             .done(funtion(data){
+              //console.log(data);
+              let feeds = data.feeds;
+              console.log(feeds[0]);
+              $("#lastTemperature").text(feed[0].field2+" C");
+              $("#lastHumidity").text(feed[0].field1+" %");
+              $("#lastupdate").text(feed[0].created_at);
+
+         });
+       });
+
    </script>
 </html>
